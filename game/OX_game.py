914442948ce4aed2b0093c14dda80b1a96bf9d7f
DOKODOKO1234 <e2110121@gl.cc.uec.ac.jp>
2024-12-06 20:32:02 +0900

    テストが通るように修正、current＿playerとmake＿moveを追加

--- game/OX_game.py
+++ game/OX_game.py
@@ -2,7 +2,23 @@ class OXGame:
 
     def __init__(self):
         self.board = [['-'] * 3 for _ in range(3)]
+        self.current_player = 'X'
 
     def display_board(self):
         for row in self.board:
             print(" ".join(row))
+
+    def make_move(self, x,y):
+        if 0 <= x < 3 and 0 <= y < 3:
+            if self.board[x][y] != '-':
+                print("Already Occupied !!")
+                return False
+            else:
+                self.board[x][y] = self.current_player
+                self.current_player = 'O' if self.current_player == 'X' else 'X'
+                return True
+        else:
+            print("Invalid Cordinates !!")
+            return False
+
+                
